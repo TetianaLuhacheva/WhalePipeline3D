246f5e0bafa115ddb0542c9d22016a7cd583203b
terra <tetiana.luhacheva@hneu.net>
2022-01-22 22:29:21 +0100

    creation of empty project

---
+++ src/main/java/eng/tetiana_luhacheva/whalepipeline3d/HelloController.java
@@ -0,0 +1,14 @@
+package eng.tetiana_luhacheva.whalepipeline3d;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module eng.tetiana_luhacheva.whalepipeline3d {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens eng.tetiana_luhacheva.whalepipeline3d to javafx.fxml;
+    exports eng.tetiana_luhacheva.whalepipeline3d;
+}
